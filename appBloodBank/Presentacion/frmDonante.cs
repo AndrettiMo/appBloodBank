eacc959d8e5d1c8cda69356106a91ecd61f2eb20
fabian <famolano18@misena.edu.co>
2021-04-28 15:29:43 -0500

    Se crearon los formulario para el registro

--- appBloodBank/Presentacion/frmDonante.cs
+++ appBloodBank/Presentacion/frmDonante.cs
@@ -23,6 +23,7 @@ namespace appBloodBank.Presentacion
             InitializeComponent();
         }
 
+
         clConexion conexion = new clConexion();
 
         private void frmDonante_Load(object sender, EventArgs e)
@@ -34,18 +35,17 @@ namespace appBloodBank.Presentacion
         {
             clMostrar objDonante = new clMostrar();
 
-           
-            RegistrarDonante = objDonante.mtdListar();
+
+
 
             dgvDonante.DataSource = RegistrarDonante;
 
         }
+        clDonante objRegistrar = new clDonante();
 
-        
-
-        private void btnGuardar_Click_1(object sender, EventArgs e)
+        public void mtdCargar()
         {
-            clDonante objRegistrar = new clDonante();
+
 
             objRegistrar.Nombre = txtNombre.Text;
             objRegistrar.Apellido = txtApellido.Text;
@@ -61,11 +61,37 @@ namespace appBloodBank.Presentacion
             objRegistrar.Telefono = txtTelefono.Text;
             objRegistrar.Email = txtEmail.Text;
 
+        }
+
+
+        public void mtdLimpiar()
+        {
+            txtNombre.Clear();
+            txtApellido.Clear();
+            txtDocumento.Clear();
+            txtEdad.Clear();
+            txtPeso.Clear();
+            txtPresion.Clear();
+            txtTemperatura.Clear();
+            txtTelefono.Clear();
+            txtEmail.Clear();
+
+
+        }
+
+
+
+        private void btnGuardar_Click_1(object sender, EventArgs e)
+        {
+            mtdCargar();
+
             int Registro = 0;
             Registro = objRegistrar.mtdRegistrar();
             if (Registro > 0)
             {
                 MessageBox.Show("Datos insertados correctamente");
+                mtdRecargarGrid();
+                mtdLimpiar();
             }
 
             else
@@ -74,6 +100,94 @@ namespace appBloodBank.Presentacion
             }
 
 
+        }
+
+        public void mtdRecargarGrid()
+        {
+            List<clDonante> listaDonante = new List<clDonante>();
+            listaDonante = objDonante.mtdListar();
+            dgvDonante.DataSource = listaDonante;
+        }
+
+
+        private void frmDonante_Load_1(object sender, EventArgs e)
+        {
+            mtdRecargarGrid();
+
+        }
+
+        int idDonante = 0;
+        private void dgvDonante_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            if (dgvDonante.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
+            {
+
+                dgvDonante.CurrentRow.Selected = true;
+                idDonante = int.Parse(dgvDonante.Rows[e.RowIndex].Cells[0].FormattedValue.ToString());
+                txtNombre.Text = dgvDonante.Rows[e.RowIndex].Cells[1].FormattedValue.ToString();
+                txtApellido.Text = dgvDonante.Rows[e.RowIndex].Cells[2].FormattedValue.ToString();
+                txtDocumento.Text = dgvDonante.Rows[e.RowIndex].Cells[3].FormattedValue.ToString();
+                dtpFecha.Value = DateTime.Parse(dgvDonante.Rows[e.RowIndex].Cells[4].FormattedValue.ToString());
+                txtEdad.Text = dgvDonante.Rows[e.RowIndex].Cells[5].FormattedValue.ToString();
+                txtPeso.Text = dgvDonante.Rows[e.RowIndex].Cells[6].FormattedValue.ToString();
+                cmbGenero.Text = dgvDonante.Rows[e.RowIndex].Cells[7].FormattedValue.ToString();
+                cmbRh.Text = dgvDonante.Rows[e.RowIndex].Cells[8].FormattedValue.ToString();
+                txtPresion.Text = dgvDonante.Rows[e.RowIndex].Cells[9].FormattedValue.ToString();
+                txtTemperatura.Text = dgvDonante.Rows[e.RowIndex].Cells[10].FormattedValue.ToString();
+                txtTelefono.Text = dgvDonante.Rows[e.RowIndex].Cells[11].FormattedValue.ToString();
+                txtEmail.Text = dgvDonante.Rows[e.RowIndex].Cells[12].FormattedValue.ToString();
+
+
+
+
+            }
+
+
+
+        }
+
+
+        public void mtdComprobar(int filas)
+        {
+            if (filas > 0)
+            {
+                MessageBox.Show("Proceso Realizado Exitosamente", "Succesful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                mtdRecargarGrid();
+                mtdLimpiar();
+
+            }
+            else
+            {
+                MessageBox.Show("No Se Pudo Realizar El Proceso", "Succesful", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        
+       
+
+        private void Modificar_Click(object sender, EventArgs e)
+        {
+            mtdCargar();
+            int filas = objRegistrar.mtdActualizar();
+            mtdComprobar(filas);
+        }
+
+        private void btnEliminar_Click_1(object sender, EventArgs e)
+        {
+
+            objDonante.idDonante = idDonante;
+            DialogResult respuesta = MessageBox.Show("Desea Eliminar el Registro", "Pregunta", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+
+            int filas = 0;
+            if (respuesta == DialogResult.OK)
+            {
+                filas = objDonante.mtdEliminar();
+
+            }
+            mtdComprobar(filas);
         }
     }
 }
--- appBloodBank/Program.cs
+++ appBloodBank/Program.cs
@@ -17,7 +17,7 @@ namespace appBloodBank
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmPrincipal());
+            Application.Run(new frmDonante());
         }
     }
 }
